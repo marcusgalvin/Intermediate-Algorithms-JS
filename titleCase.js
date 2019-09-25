8eaded82ca5a53e7d82a9ee38b8ca2ae345167d1
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-09-25 13:52:21 -0400

    so close

--- titleCase.js
+++ titleCase.js
@@ -1,6 +1,6 @@
 function titleCase(str) {
   var result = "";
-  //first make every word in str lower case
+  //first make every word in str lower case to make this a little easier
   var lowerCase = str.toLowerCase();
   console.log(lowerCase);
 
@@ -8,14 +8,17 @@ function titleCase(str) {
   var split = lowerCase.split(" ");
   console.log(split);
 
-  // next locate the first char in each substring
-  var firstLetterFinder = split[0][0];
-  console.log(firstLetterFinder);
+  for (var i = 0; i < split.length; i++) {
+    // next locate the first char in each substring
+    //this variable iterates through each substring and then acces the first char by using [0]
+    var firstLetterFinder = split[i][0];
+    // console.log(firstLetterFinder)
 
-  var finalResult = firstLetterFinder.toUpperCase();
-  console.log(finalResult);
+    var finalResult = firstLetterFinder.toUpperCase();
+    console.log(finalResult);
+  }
 
-  return str;
+  return finalResult;
 }
 
 titleCase("I'm a little tea pot");
