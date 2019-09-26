9bdae2318b53c580a38463f695cedf1484c9bc50
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-09-26 11:45:51 -0400

    solved

--- titleCase.js
+++ titleCase.js
@@ -1,12 +1,15 @@
 function titleCase(str) {
+  //check test case 2
+  console.log(str);
+
   var result = "";
   //first make every word in str lower case to make this a little easier
   var lowerCase = str.toLowerCase();
-  console.log(lowerCase);
+  // console.log(lowerCase)
 
   //split into array
   var split = lowerCase.split(" ");
-  console.log(split);
+  // console.log(split)
 
   for (var i = 0; i < split.length; i++) {
     // next locate the first char in each substring
@@ -14,11 +17,31 @@ function titleCase(str) {
     var firstLetterFinder = split[i][0];
     // console.log(firstLetterFinder)
 
-    var finalResult = firstLetterFinder.toUpperCase();
-    console.log(finalResult);
+    //capitalize the first letter
+    var capital = firstLetterFinder.toUpperCase();
+    // console.log(capital)
+
+    //concatinate the new capital letter with each substring
+    var concatCapital = capital.concat(split[i]);
+    // console.log(concatCapital)
+
+    var test = concatCapital.slice(2);
+    // console.log("test",test)
+
+    var testConCat = capital.concat(test);
+    // console.log(testConCat)
+
+    var results = testConCat.toString();
+    console.log(results);
+
+    //now we need to remove the second index of each substring
+    // console.log(concatCapital[1][0])
+
+    // finalResult = concatCapital.slice(1);
+    // console.log(finalResult)
   }
 
-  return finalResult;
+  return results;
 }
 
-titleCase("I'm a little tea pot");
+titleCase("sHoRt AnD sToUt");
